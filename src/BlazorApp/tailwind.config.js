439061b9d1a8de302d1460903071941e15b2ebeb
Ahmad Hanif Nurfauzi <hanif_a13@yahoo.com>
2024-05-02 15:40:30 +0700

    Tailwind Attempt 1 Failed

---
+++ src/BlazorApp/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./components/razor',
+            './Layout/razor',
+            './pages/razor',
+            './razor'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
